b2615a383a3cd11b3e30578eb5776c90734d1ca9
Aaron Stoiano <aaronstoiano@gmail.com>
2020-10-06 11:44:11 -0400

    added the generation code for genMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,54 @@
 function generateMarkdown(data) {
+return ` # ${data.title}
 
-}
+
+    ## Table Of Contents
+1. [Installation](#installation)
+2. [Usage](usage)
+3. [License](#license)
+4. [Contributing](#contributing)
+5. [Tests](#tests)
+6. [Questions](#questions)
+
+
+
+https://img.shields.io/badge/License-MIT-Blue - automatic!
+
+(#Title)
+
+${data.title}
+
+(#Description)
+
+${data.description}
+
+(#Table of Contents)
+
+[Installation](#Installation)
+
+${data.installation}
+
+[Usage](#Usage)
+
+${data.usage}
+
+[License](#License)
+
+${data.license}
+
+[Contributing](#Contributing)
+
+${data.contributing}
+
+[Tests](#Tests)
+
+${data.tests}
+
+[Questions](#Questions)
+
+${data.questions}
+
+`
+};
 
 module.exports = generateMarkdown;
